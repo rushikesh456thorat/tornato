5669549a49af88f1e515c56dd303d763a33b58ad
rushikesh456thorat <80233861+rushikesh456thorat@users.noreply.github.com>
2022-08-12 17:37:00 +0530

    Update getData.js

--- scripts/getData.js
+++ scripts/getData.js
@@ -5,85 +5,80 @@
 		function setErrorDisplay(){
 			loaderElm.style.display = "none"
 			allNamesElm.style.display = "none"
-			errorMessageElm.style.display = "block"
+			errorMessageElm.style.display = "flex"
 		}
-			
-		fetch("https://api.apispreadsheets.com/data/CfJHtyhSrpH0uzaX/").then(res=>{
-			if (res.status === 200){
-				res.json().then(data=>{
-					const yourData = data["data"]
-					for(let i = 0; i < yourData.length; i++){
-						let rowInfo = yourData[i]
-
-						loaderElm.style.display = "none"
-					    allNamesElm.style.display = "block"
-					   errorMessageElm.style.display = "none"
-
-					    let anchor=document.createElement("a")
-						anchor.href= rowInfo["downloadLink"]
-						anchor.target="_blank"
-
-						let rowInfoDiv = document.createElement("span")
-						rowInfoDiv.classList.add("bookInfoContainer")
-                        rowInfoDiv.style="margin-top:5px ;"
-						rowInfoDiv.display="inline"
-						
-						
-                        
-                        let imageOfBook = document.createElement("img")
-                        imageOfBook.src = new URL(rowInfo["imageSource"])
-
-
-
-						let rowName = document.createElement("h3")
-						let rowNameNode = document.createTextNode(rowInfo["bookTitle"])
-						rowName.appendChild(rowNameNode)
-                        rowName.id="book-title"
-                        rowName.style="margin: 5px;"
-                        rowName.title="Book Title"
-						console.log(rowInfo["bookTitle"])
-
-                        let bookTitle = document.createElement("h4")
-						let bookTitleNode = document.createTextNode(rowInfo["Author"])
-						bookTitle.appendChild(bookTitleNode)
-                        bookTitle.id="Author"
-                        bookTitle.style="margin:5px;color:rgb(96, 113, 55); "
-
-						let rowWritten = document.createElement("p")
-						let rowWrittenNode = document.createTextNode(rowInfo["downloadSize"])
-						rowWritten.appendChild(rowWrittenNode)
-						rowWritten.classList.add("download-size")
-                        rowWrittenNode.style="margin: 5px; color: green; font-family: Arial, Helvetica, sans-serif;"
-
-						
-						let rowBook = document.createElement("div")
-						rowBook.id = "book-Info"
-						rowBook.classList.add("book-Info")
-                        rowBook.appendChild(rowName)
-                        rowBook.appendChild(bookTitle)
-                        rowBook.appendChild(rowWritten)
-
-
-						rowInfoDiv.appendChild(imageOfBook)
-						rowInfoDiv.appendChild(rowBook)
-						anchor.appendChild(rowInfoDiv)
-						allNamesElm.appendChild(anchor)
-
-						
-						
-
-					}
-					
-					
-
-				}).catch(err => {
-					setErrorDisplay()
-					console.log(err);
-				})
-			}
-			else{
+		
+		 $.ajax({
+			// http may be used instead of https if required
+			url: "https://sheetlabs.com/MRE/bookdata",
+			crossDomain : true,
+		  })
+		  .done(function(data) {
+			if (data.length == 0) {
 				setErrorDisplay()
+			  return;
 			}
-		}).catch(err =>{
+			$.each(data, function(key, value) {
+			   console.log(value.imagesource);
+
+			   loaderElm.style.display = "none"
+			   allNamesElm.style.display = "block"
+			   errorMessageElm.style.display = "none"
+
+			   let anchor=document.createElement("a")
+			   anchor.href= value.downloadlink
+			   anchor.target="_blank"
+
+			   let rowInfoDiv = document.createElement("span")
+			   rowInfoDiv.classList.add("bookInfoContainer")
+			   rowInfoDiv.style="margin-top:5px ;"
+			   rowInfoDiv.display="inline"
+			   
+			   
+			   
+			   let imageOfBook = document.createElement("img")
+			   imageOfBook.src = new URL(value.imagesource)
+
+
+
+			   let rowName = document.createElement("h3")
+			   let rowNameNode = document.createTextNode(value.booktitle)
+			   rowName.appendChild(rowNameNode)
+			   rowName.id="book-title"
+			   rowName.style="margin: 5px;"
+			   rowName.title="Book Title"
+			   
+
+			   let bookTitle = document.createElement("h4")
+			   let bookTitleNode = document.createTextNode(value.author)
+			   bookTitle.appendChild(bookTitleNode)
+			   bookTitle.id="Author"
+			   bookTitle.style="margin:5px;color:rgb(96, 113, 55); "
+
+			   let rowWritten = document.createElement("p")
+			   let rowWrittenNode = document.createTextNode(value.downloadsize)
+			   rowWritten.appendChild(rowWrittenNode)
+			   rowWritten.classList.add("download-size")
+			   rowWrittenNode.style="margin: 5px; color: green; font-family: Arial, Helvetica, sans-serif;"
+
+			   
+			   let rowBook = document.createElement("div")
+			   rowBook.id = "book-Info"
+			   rowBook.classList.add("book-Info")
+			   rowBook.appendChild(rowName)
+			   rowBook.appendChild(bookTitle)
+			   rowBook.appendChild(rowWritten)
+
+
+			   rowInfoDiv.appendChild(imageOfBook)
+			   rowInfoDiv.appendChild(rowBook)
+			   anchor.appendChild(rowInfoDiv)
+			   allNamesElm.appendChild(anchor)
+			});
+		  })
+		  .fail(function() {
 			setErrorDisplay()
-		})
+		  });
+			
+		
+		
